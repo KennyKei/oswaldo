e6e8c8cb78da5ea4a7f8d7acea7a222b098fd954
Kenny Keisuke Hobo <keisuke23@hotmail.com>
2023-08-22 08:39:18 -0300

    fist commit

---
+++ bin/exemplo01.dart
@@ -0,0 +1,5 @@
+import 'package:exemplo01/exemplo01.dart' as exemplo01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exemplo01.calculate()}!');
+}
---
+++ test/exemplo01_test.dart
@@ -0,0 +1,8 @@
+import 'package:exemplo01/exemplo01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
